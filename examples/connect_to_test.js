7f5b4b0d4a9f216ecc5463ee249826c39baad861
viRingbells <viRingbells@qq.com>
2015-02-14 13:57:03 +0800

    Version Up, add attach, support socket.io

--- examples/connect_to_test.js
+++ examples/connect_to_test.js
@@ -1,30 +1,9 @@
 var net = require('net');
 
-function connect(timeout){
-    var socket = net.connect(8300);
+var socket = net.connect(8300);
 
-    socket.on('error',function(e){
-        console.log(e);
-    });
+socket.write("How are you");
 
-    socket.on('connect',function(){
-        console.log("connected");
-    });
-
-    socket.on('data',function(data){
-        console.log(data.toString());
-    });
-
-    socket.on('end',function(){
-        console.log("disconnected");
-    });
-
-    socket.write("How are you");
-
-    setTimeout(function(){
-        socket.end();
-    },timeout * 1000);
-};
-
-connect(8);
-connect(4);
+setTimeout(function(){
+    socket.end();
+},2000);
---
+++ examples/get_started.js
@@ -0,0 +1,17 @@
+var io  = require('..');
+
+var app = io();
+
+app.use(function*(next){
+    console.log('Connect');
+    yield next;
+    console.log('Disconnect');
+});
+
+app.route('data', function*(next){
+    console.log("Receive : " + this.args[0]);
+});
+
+app.listen(8300,function(){
+    console.log("Listening at 8300...");   
+});
