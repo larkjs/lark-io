daddb3a3cbb4b8e3a88b66cac8a5cdee37b979b1
mbu-se-vc <mbu-se-vc@mbu-se-vcdeiMac.local>
2015-02-28 16:51:54 +0800

    add intervene for socket.io

--- examples/socket.io.adapt.js
+++ examples/socket.io.adapt.js
@@ -1,5 +1,6 @@
 var io  = require('..');
 var fs  = require('fs');
+var redis = require('socket.io-redis');
 
 var realtime = io();
 var app      = io();
@@ -8,8 +9,14 @@ app.io(io.http);
 realtime.io(io.socketio);
 app.attach(realtime);
 
+realtime.socketio.init(function(io){
+    console.log("Adapting");
+    io.adapter(redis({ host: 'localhost', port: 6379 }));
+});
+
 realtime.use(function*(next){
     console.log('Connect');
+    console.log(process.pid);
     yield next;
     console.log('Disconnect');
 });
